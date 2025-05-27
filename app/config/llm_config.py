18267cd9db9b9b0d2d9705e7cee8ec80ae22baac
juan <juan.bisposantos05@gmail.com>
2025-05-27 16:13:00 -0300

    Alterando os prompts de delivery coordinator, adicionando etapas lógicas e adicionando envio de mensagens ao callbell, ativando stream

--- app/config/llm_config.py
+++ app/config/llm_config.py
@@ -5,4 +5,5 @@ from app.config.settings import settings
 default_llm = LLM(
     model='xai/grok-3',
     api_key=settings.XAI_API_KEY,
+    stream=True
     )
--- app/tools/callbell_tools.py
+++ app/tools/callbell_tools.py
@@ -1,42 +1,46 @@
 from crewai.tools import BaseTool
-from typing import Type, Any, Dict
+from typing import Type, Any, Dict, List
 from pydantic import BaseModel, Field
 from app.config.settings import settings
 import requests
 
+from time import sleep
 
-class CallbellSendInfo(BaseModel):
+class CallbellSendInput(BaseModel):
     """Modelo para enviar informações via Callbell."""
     phone_number: str = Field(..., description="Número de telefone do destinatário")
-    message: str = Field(..., description="Mensagem a ser enviada")
+    messages: str = Field(..., description="Mensagens a ser enviadas")
 
 class CallbellSendTool(BaseTool):
     name: str = "CallbellSendTool"
-    description: str = "Envia uma mensagem via Callbell para um número de telefone específico."
-    args_schema: Type[BaseModel] = CallbellSendInfo
+    description: str = "Envia uma mensagem via Callbell para um número de telefone específico"
+    args_schema: Type[BaseModel] = CallbellSendInput
     
-    def _run(self, phone_number: str, message: str) -> Dict[str, Any]:
+    def _run(self, phone_number: str, messages: str) -> Dict[str, Any]:
         """Envia uma mensagem via Callbell."""
-        url = "https://api.callbell.eu/v1/messages/send"
-        headers = {
-            "Authorization": f"Bearer {settings.CALLBELL_API_KEY}",
-            "Content-Type": "application/json"
-        }
         
-        payload = {
-            "to": phone_number,
-            "from": "whatsapp",
-            "type": "text",
-            "channel_uuid": "b3501c231325487086646e19fc647b0d",
-            "content": {
-                "text": message
-            },
-            "filds": "conversation,contact"
-        }
-        
-        response = requests.post(url, json=payload, headers=headers)
-        
-        if response.status_code == 200:
-            return {"status": "success", "data": response.json()}
-        else:
-            return {"status": "error", "message": response.text}
+        for message in messages:
+            sleep(0.5)
+            url = "https://api.callbell.eu/v1/messages/send"
+            headers = {
+                "Authorization": f"Bearer {settings.CALLBELL_API_KEY}",
+                "Content-Type": "application/json"
+            }
+            
+            payload = {
+                "to": phone_number,
+                "from": "whatsapp",
+                "type": "text",
+                "channel_uuid": "b3501c231325487086646e19fc647b0d",
+                "content": {
+                    "text": message
+                },
+                "filds": "conversation,contact"
+            }
+            
+            response = requests.post(url, json=payload, headers=headers)
+            
+            if response.status_code == 200:
+                return {"status": "success", "data": response.json()}
+            else:
+                return {"status": "error", "message": response.text}
