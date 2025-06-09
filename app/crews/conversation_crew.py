cd31e8fddbbd5e34cd9566de8fa5aa4e06bca2b6
juan <juan.bisposantos05@gmail.com>
2025-06-09 09:39:22 -0300

    Alterando fluxo, criando função para parse e refinando a nova estrategia de estado de conversa

--- app/crews/conversation_crew.py
+++ app/crews/conversation_crew.py
@@ -30,6 +30,7 @@ from app.tools.callbell_tools import CallbellSendTool
 
 from app.services.qdrant_service import get_client
 from app.services.telegram_service import send_single_telegram_message
+from app.services.state_manager_service import StateManagerService
 
 import litellm
 
@@ -68,6 +69,30 @@ def patched_completition(*args, **kwargs):
 
 litellm.completion = patched_completition
 
+state_manager = StateManagerService()
+
+
+def parse_json_from_string(json_string):
+    try:
+        if '```json' in json_string:
+                    json_string = json_string.split('```json')[-1].split('```')[0]
+                
+        if '```' in json_string:
+            json_string = json_string.replace('```', '')
+    
+        json_response = json.loads(json_string)
+
+        if 'task_output' and 'updated_state' in json_response:
+            task_output = json_response['task_output']
+            updated_state = json_response['updated_state']
+
+            return task_output, updated_state
+        
+        return json_response, None
+    except json.JSONDecodeError as e:
+        return None, None
+    
+
 def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, history: Any):
 
     # litellm._turn_on_debug()
@@ -82,7 +107,10 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
     history_messages = ''
     if history:
         history_messages = '\n'.join([f'{"AI" if "Alessandro" in str(message.get("text", "")) else "collaborator" if not message.get("status", "") == "received" else "customer"} - {message.get("text")}' if message.get("text") else '' for message in reversed(history.get('messages', [])[:10])])
-         
+    
+    state = state_manager.get_state(contact_id)
+
+    state["metadata"]["current_turn_number"] += 1
 
     jump_to_registration_task = False
     registration_task = False
@@ -113,7 +141,9 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
         "timestamp": datetime.datetime.now().isoformat(), 
         "l0l1_cache": str(L1CacheQueryTool()._run(contact_id)),
         "l2_cache": str(FastMemoryMessages()._run(contact_id)),
-        "history": history_messages
+        "history": history_messages,
+        "conversation_state": str(state),
+        "turn": state["metadata"]["current_turn_number"]
         }
 
         logger.info(f"MVP Crew: Executando kickoff com inputs: {inputs_triage}")
@@ -124,18 +154,7 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
         logger.info(f"MVP Crew: Resposta gerada pelo crew: {response_triage}")
 
         if response_triage:
-            json_response = None
-            try:
-                if '```json' in response_triage:
-                    response_triage = response_triage.split('```json')[-1].split('```')[0]
-                
-                if '```' in response_triage:
-                    response_triage = response_triage.replace('```', '')
-                    
-                json_response = json.loads(response_triage)
-                
-            except json.JSONDecodeError:
-                logger.error(f"MVP Crew: Resposta da triagem não é um JSON válido: {response_triage}")
+            json_response, updated_state = parse_json_from_string(response_triage)
             
             if json_response:
         
@@ -145,6 +164,10 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
                 if 'operational_context' in json_response and json_response['operational_context'] == 'BUDGET_ACCEPTED':
                     registration_task = True
                     redis_client.set(f"{contact_id}:plan_details", json_response.get('plan_details', ""))
+
+            if updated_state and isinstance(updated_state, dict):
+                state_manager.save_state(contact_id, updated_state)
+                state = updated_state
 
         else:
             logger.warning(f"MVP Crew: Nenhuma resposta gerada para contact_id {contact_id}")
@@ -181,7 +204,10 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
                 "identified_topic": json_response.get('identified_topic', ''),
                 "customer_profile": str(GetUserProfile()._run(contact_id)),
                 "history": history_messages,
-                "contact_id": contact_id
+                "contact_id": contact_id,
+                "conversation_state": str(state),
+                "timestamp": datetime.datetime.now().isoformat(),
+                "turn": state["metadata"]["current_turn_number"]
             }
             
             new_profile_crew.kickoff(inputs=inputs_profile)
@@ -253,24 +279,19 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
             "history": history_messages,
             "message_text_original": '\n'.join(redis_client.lrange(f'contacts_messages:waiting:{contact_id}', 0, -1)),
             "collected_data_so_far": str(user_data_so_far),
-            "plan_details": str(plan_details)
+            "plan_details": str(plan_details),
+            "turn": state["metadata"]["current_turn_number"]
         }
 
         registration_crew.kickoff(inputs_for_registration)
         
         registration_task_str = registration_task.output.raw
-        registration_task_json = None
-        try:
-            if '```json' in registration_task_str:
-                    registration_task_str = registration_task_str.split('```json')[-1].split('```')[0]
-                
-            if '```' in registration_task_str:
-                registration_task_str = registration_task_str.replace('```', '')
-                
-            registration_task_json = json.loads(registration_task_str)
-        except json.JSONDecodeError:
-            pass
+        registration_task_json, updated_state = parse_json_from_string(registration_task_str)
         
+        if updated_state and isinstance(updated_state, dict):
+            state = updated_state
+            state_manager.save_state(contact_id, state)
+
         if registration_task_json and all([registration_task_json.get("is_data_collection_complete"), registration_task_json.get("status") == 'COLLECTION_COMPLETE']):
             send_single_telegram_message(registration_task_str, '-4854533163')
             redis_client.delete(f"{contact_id}:getting_data_from_user")
@@ -313,7 +334,10 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
                 "operational_context": json_response.get('operational_context', ''),
                 "identified_topic": json_response.get('identified_topic', ''),
                 "customer_profile": str(GetUserProfile()._run(contact_id)),
-                "history": history_messages
+                "history": history_messages,
+                "conversation_state": str(state),
+                "timestamp": datetime.datetime.now().isoformat(),
+                "turn": state["metadata"]["current_turn_number"]
             }
             
             crew_profile.kickoff(inputs_profile)
@@ -330,11 +354,19 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
                 verbose=True
             )
             
+            profile_customer_task_json, updated_state = parse_json_from_string(profile_customer_task.output.raw)
+            if updated_state and isinstance(updated_state, dict):
+                state = updated_state
+                state_manager.save_state(contact_id, state)
+
             inputs_strategic = {
-                "profile_customer_task_output": profile_customer_task.output.raw,
+                "profile_customer_task_output": str(profile_customer_task_json),
                 "message_text_original": '\n'.join(redis_client.lrange(f'contacts_messages:waiting:{contact_id}', 0, -1)),
                 "operational_context": json_response.get('operational_context', ''),
                 "identified_topic": json_response.get('identified_topic', ''),
+                "conversation_state": str(state),
+                "timestamp": datetime.datetime.now().isoformat(),
+                "turn": state["metadata"]["current_turn_number"]
             }
             
             crew_strategic.kickoff(inputs_strategic)
@@ -342,25 +374,19 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
             # Saving Profile and Strategic Plan
             qdrant_client = get_client()
             
-            tasks_map = {
-                'profile_customer': profile_customer_task.output.raw,
-                'strategic_plan': strategic_advise_task.output.raw
+            strategic_advise_task_str = strategic_advise_task.output.raw
+            strategic_advise_task_json, updated_state = parse_json_from_string(strategic_advise_task_str)
+
+            if updated_state and isinstance(updated_state, dict):
+                state = updated_state
+                state_manager.save_state(contact_id, state)
+
+            payload = {
+                'profile_customer': profile_customer_task_json,
+                'strategic_plan': strategic_advise_task_json,
             }
-            
-            payload = {}
-            for name_task, response_task in tasks_map.items():
-                try:
-                    if '```json' in response_task:
-                        response_task = response_task.split('```json')[-1].split('```')[0]
-                    
-                    if '```' in response_task:
-                        response_task = response_task.replace('```', '')
-                        
-                    payload[name_task] = json.loads(response_task)
-                    
-                except json.JSONDecodeError:
-                    pass
-            
+
+
             if payload:  
                 scroll = qdrant_client.scroll(
                     'UserProfiles',
@@ -405,13 +431,17 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
             for k in redis_client.keys(f"contact:{contact_id}:sendend_catalog_*"):
                 recently_sent_catalogs.append(k.split("sendend_catalog_")[-1])
             
+
             inputs_craft = {
-                "develop_strategy_task_output": strategic_advise_task.output.raw,
-                "profile_customer_task_output": profile_customer_task.output.raw,
+                "develop_strategy_task_output": str(strategic_advise_task_json),
+                "profile_customer_task_output": str(profile_customer_task_json),
                 "message_text_original": '\n'.join(redis_client.lrange(f'contacts_messages:waiting:{contact_id}', 0, -1)),
                 "operational_context": json_response.get('operational_context', ''),
                 "identified_topic": json_response.get('identified_topic', ''),
-                "recently_sent_catalogs": ', '.join(recently_sent_catalogs)
+                "recently_sent_catalogs": ', '.join(recently_sent_catalogs),
+                "conversation_state": str(state),
+                "timestamp": datetime.datetime.now().isoformat(),
+                "turn": state["metadata"]["current_turn_number"]
             }
 
             
@@ -419,17 +449,12 @@ def run_mvp_crew(contact_id: str, phone_number: str, redis_client: redis.Redis, 
             
             response_craft_json = None
             response_craft_str = response_craft_task.output.raw
-            try:
-                if '```json' in response_craft_str:
-                    response_craft_str = response_craft_str.split('```json')[-1].split('```')[0]
-                    
-                if '```' in response_craft_str:
-                    response_craft_str = response_craft_str.replace('```', '')
-                    
-                response_craft_json = json.loads(response_craft_str)
-                
-            except json.JSONDecodeError:
-                logger.error('Não foi possível parsear o json de respostas finais')
+
+            response_craft_json, updated_state = parse_json_from_string(response_craft_str)
+
+            if updated_state and isinstance(updated_state, dict):
+                state = updated_state
+                state_manager.save_state(contact_id, state)
                 
             redo = False
             if response_craft_json:
@@ -614,39 +639,35 @@ o sistema enviará o(s) catálogo(s) do(s) plano(s) {', '.join(plans_names_to_se
                 "identified_topic": json_response.get('identified_topic', ''),
                 "operational_context": json_response.get('operational_context', ''),
                 "message_text_original": '\n'.join(last_messages_processed),
-                "fast_messages": str(memory.get('primary_messages_sequence', '')),
+                "primary_messages_sequence": str(memory.get('primary_messages_sequence', '')),
                 "proactive_content_generated": str(memory.get('proactive_content_generated', '')),
                 "client_profile": str(profile.get('profile_customer', '')),
                 "strategic_plan": str(profile.get('strategic_plan', '')),
                 "history": history_messages,
                 "system_input": str(redis_client.hget(f'contact:{contact_id}', 'system_input')),
+                "timestamp": datetime.datetime.now().isoformat(),
+                "conversation_state": str(state),
+                "turn": state["metadata"]["current_turn_number"]
             }
             
                                 
             delivery_crew.kickoff(inputs_delivery_crew)
             
-            response_delivery_json = None
             response_delivery_str = delivery_coordinator_task.output.raw
-            try:
-                if '```json' in response_delivery_str:
-                    response_delivery_str = response_delivery_str.split('```json')[-1].split('```')[0]
-                
-                if '```' in response_delivery_str:
-                    response_delivery_str = response_delivery_str.replace('```', '')
-                    
-                response_delivery_json = json.loads(response_delivery_str)
-                
-            except json.JSONDecodeError:
-                logger.error(f"MVP Crew: Resposta não é um JSON válido: {response_delivery_str}")
-            
+            response_delivery_json, updated_state = parse_json_from_string(response_delivery_str)
+
+            if updated_state and isinstance(updated_state, dict):
+                state = updated_state
+                state_manager.save_state(contact_id, state)
+
             if response_delivery_json:
                 logger.info(f'[{contact_id}] - response_delivery_json existe.')
 
-                if not 'fast_messages_choosen_index' in response_delivery_json:
-                    logger.info(f'[{contact_id}] - "fast_messages_choosen_index" NÃO está em response_delivery_json. Chamando run_mvp_crew.')
+                if not 'primary_messages_sequence_choosen_index' in response_delivery_json:
+                    logger.info(f'[{contact_id}] - "primary_messages_sequence_choosen_index" NÃO está em response_delivery_json. Chamando run_mvp_crew.')
                     run_mvp_crew(contact_id, phone_number, redis_client, history)
                 else:
-                    logger.info(f'[{contact_id}] - "fast_messages_choosen_index" está em response_delivery_json. Prosseguindo com a lógica de order/Final Answer.')
+                    logger.info(f'[{contact_id}] - "primary_messages_sequence_choosen_index" está em response_delivery_json. Prosseguindo com a lógica de order/Final Answer.')
 
                 payload = point_memory.payload.copy()
                 if 'order' in response_delivery_json:
@@ -660,9 +681,9 @@ o sistema enviará o(s) catálogo(s) do(s) plano(s) {', '.join(plans_names_to_se
                     new_payload = point_memory.payload.copy()
                     logger.info(f'[{contact_id}] - Payload de point_memory copiado para new_payload.')
 
-                    if response_delivery_json['fast_messages_choosen_index']:
-                        logger.info(f'[{contact_id}] - "fast_messages_choosen_index" existe e não está vazio. Removendo mensagens primárias.')
-                        for index in response_delivery_json['fast_messages_choosen_index']:
+                    if response_delivery_json['primary_messages_sequence_choosen_index']:
+                        logger.info(f'[{contact_id}] - "primary_messages_sequence_choosen_index" existe e não está vazio. Removendo mensagens primárias.')
+                        for index in response_delivery_json['primary_messages_sequence_choosen_index']:
                             
                             try:
                                 logger.debug(f'[{contact_id}] - Removendo índice {index} de primary_messages_sequence.')
@@ -717,9 +738,9 @@ o sistema enviará o(s) catálogo(s) do(s) plano(s) {', '.join(plans_names_to_se
                         new_response_json[k] = v
                     logger.info(f'[{contact_id}] - Cópia para new_response_json concluída. Conteúdo: {list(new_response_json.keys())}.')
 
-                    if new_response_json.get('fast_messages_choosen_index'):
-                        logger.info(f'[{contact_id}] - "fast_messages_choosen_index" existe em new_response_json. Removendo mensagens primárias.')
-                        for index in new_response_json['fast_messages_choosen_index']:
+                    if new_response_json.get('primary_messages_sequence_choosen_index'):
+                        logger.info(f'[{contact_id}] - "primary_messages_sequence_choosen_index" existe em new_response_json. Removendo mensagens primárias.')
+                        for index in new_response_json['primary_messages_sequence_choosen_index']:
                             
                             try:
                                 logger.debug(f'[{contact_id}] - Removendo índice {index} de primary_messages_sequence.')
