08246fdc372142cd91b566992a0d6c0a5736bafe
juan <juan.bisposantos05@gmail.com>
2025-06-23 15:03:43 -0300

    trocando o LLM de context analisys para o da openai, e adicionando as tools também para a declaração do agente

--- app/agents/agent_declaration.py
+++ app/agents/agent_declaration.py
@@ -5,7 +5,8 @@ from app.config.llm_config import *
 
 from app.utils.funcs.funcs import obter_caminho_projeto
 
-from app.tools.knowledge_tools import KnowledgeServiceTool
+from app.tools.knowledge_tools import knowledge_service_tool
+from app.tools.system_operations_tools import system_operations_tool
 
 
 base_path = obter_caminho_projeto()
@@ -13,29 +14,28 @@ config_path = base_path / 'app/config/crew_definitions/agents.yaml'
 
 agents_config = yaml.safe_load(open(config_path, 'r').read())
 
-
-
 def get_context_analysis_agent() -> Agent:
     return Agent(
         config=agents_config['ContextAnalysisAgent'],
-        llm=fast_reasoning_X_llm,
+        llm=default_openai_llm,
         verbose=True,
         allow_delegation=False,
     )
 
-def get_strategic_advisor_agent(llm = None) -> Agent:
+def get_strategic_advisor_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['StrategicAdvisor'],
         llm=default_openai_llm if not llm else llm,
-        tools=[KnowledgeServiceTool()],
+        tools=[knowledge_service_tool],
         verbose=True,
         allow_delegation=False,
     )
 
-def get_system_operations_agent() -> Agent:
+def get_system_operations_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['SystemOperationsAgent'],
-        llm=fast_reasoning_X_llm,
+        llm=default_openai_llm if not llm else llm,
+        tools=[system_operations_tool],
         verbose=True,
         allow_delegation=False,
     )
