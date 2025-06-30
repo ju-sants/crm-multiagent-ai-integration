b2cc25f4e2ab16ac1b967aff54bfa9fe1dc023c8
juan <juan.bisposantos05@gmail.com>
2025-06-30 16:04:02 -0300

    trocando LLMs dos agentes para ficarem de acordo com suas tarefas, divididos entre os LLMs criativos e os decisivos

--- app/agents/agent_declaration.py
+++ app/agents/agent_declaration.py
@@ -14,7 +14,7 @@ agents_config = yaml.safe_load(open(config_path, 'r').read())
 def get_context_analysis_agent() -> Agent:
     return Agent(
         config=agents_config['ContextAnalysisAgent'],
-        llm=default_openai_llm,
+        llm=decivise_openai_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -22,7 +22,7 @@ def get_context_analysis_agent() -> Agent:
 def get_strategic_advisor_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['StrategicAdvisor'],
-        llm=default_openai_llm if not llm else llm,
+        llm=creative_openai_llm if not llm else llm,
         tools=[knowledge_service_tool, drill_down_topic_tool],
         verbose=True,
         allow_delegation=False,
@@ -31,7 +31,7 @@ def get_strategic_advisor_agent(llm=None) -> Agent:
 def get_system_operations_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['SystemOperationsAgent'],
-        llm=default_openai_llm if not llm else llm,
+        llm=decivise_openai_llm if not llm else llm,
         tools=[system_operations_tool],
         verbose=True,
         allow_delegation=False,
@@ -40,7 +40,7 @@ def get_system_operations_agent(llm=None) -> Agent:
 def get_communication_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['CommunicationAgent'],
-        llm=default_openai_llm if not llm else llm,
+        llm=creative_openai_llm if not llm else llm,
         tools=[drill_down_topic_tool],
         verbose=True,
         allow_delegation=False,
@@ -49,7 +49,7 @@ def get_communication_agent(llm=None) -> Agent:
 def get_registration_agent() -> Agent:
     return Agent(
         config=agents_config['RegistrationDataCollectorAgent'],
-        llm=default_openai_llm,
+        llm=decivise_openai_llm,
         verbose=True,
         allow_delegation=False
     )
@@ -57,7 +57,7 @@ def get_registration_agent() -> Agent:
 def get_history_summarizer_agent() -> Agent:
     return Agent(
         config=agents_config['HistorySummarizerAgent'],
-        llm=default_openai_llm,
+        llm=decivise_openai_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -65,7 +65,7 @@ def get_history_summarizer_agent() -> Agent:
 def get_data_quality_agent() -> Agent:
     return Agent(
         config=agents_config['DataQualityAgent'],
-        llm=default_openai_llm,
+        llm=decivise_openai_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -73,7 +73,7 @@ def get_data_quality_agent() -> Agent:
 def get_state_summarizer_agent() -> Agent:
     return Agent(
         config=agents_config['StateSummarizerAgent'],
-        llm=default_openai_llm,
+        llm=decivise_openai_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -81,7 +81,7 @@ def get_state_summarizer_agent() -> Agent:
 def get_profile_enhancer_agent() -> Agent:
     return Agent(
         config=agents_config['ProfileEnhancerAgent'],
-        llm=default_openai_llm,
+        llm=creative_openai_llm,
         verbose=True,
         allow_delegation=False,
     )
