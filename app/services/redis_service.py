ebd904f897235a9c309681bdc216459f9919806f
juan <juan.bisposantos05@gmail.com>
2025-07-10 10:01:21 -0300

    alterando para que a func use por padrao o DB definido em settings

--- app/services/redis_service.py
+++ app/services/redis_service.py
@@ -12,7 +12,7 @@ def get_redis(db=None):
     redis_conn = None
     
     try:
-        redis_conn = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD, db=5 if not db else db, decode_responses=True)
+        redis_conn = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD, db=settings.REDIS_DB_MAIN if not db else db, decode_responses=True)
         redis_conn.ping()
         logger.info(f"Successfully connected to Redis DB {settings.REDIS_DB_MAIN}")
     except redis.exceptions.ConnectionError as e:
