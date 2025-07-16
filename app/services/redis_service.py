948c9d0900ab03092ee6ca4bf9e9345a32cc7343
juan <juan.bisposantos05@gmail.com>
2025-07-16 17:29:16 -0300

    ...

--- app/services/redis_service.py
+++ app/services/redis_service.py
@@ -6,17 +6,16 @@ from functools import lru_cache
 
 logger = get_logger(__name__)
 
-
 @lru_cache(maxsize=1)
-def get_redis(db=None, host=None, port=None, password=None):
+def get_redis(db: int | None = None, host: str | None = None, port: int | None = None, password: str | None = None):
     redis_conn = None
     
     try:
         redis_conn = redis.Redis(host=settings.REDIS_HOST if not host else host, port=settings.REDIS_PORT if not port else port, password=settings.REDIS_PASSWORD if not password else password, db=settings.REDIS_DB_MAIN if not db else db, decode_responses=True)
         redis_conn.ping()
         logger.info(f"Successfully connected to Redis DB {settings.REDIS_DB_MAIN}")
-    except redis.exceptions.ConnectionError as e:
+    except redis.ConnectionError as e:
         logger.error(f"Failed to connect to Redis DB {settings.REDIS_DB_MAIN}: {e}")
         exit(1)
-        
+
     return redis_conn
