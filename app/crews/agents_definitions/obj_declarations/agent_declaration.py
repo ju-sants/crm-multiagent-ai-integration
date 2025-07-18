59696d5e704222a6b7749584c5d0494410b75439
juan <juan.bisposantos05@gmail.com>
2025-07-18 17:03:37 -0300

    trocando o LLM dos agentes para o grok 3 mini fast

--- app/crews/agents_definitions/obj_declarations/agent_declaration.py
+++ app/crews/agents_definitions/obj_declarations/agent_declaration.py
@@ -14,7 +14,7 @@ agents_config = yaml.safe_load(open(config_path, 'r').read())
 def get_routing_agent() -> Agent:
     return Agent(
         config=agents_config['RoutingAgent'],
-        llm=decivise_openai_llm,
+        llm=X_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -22,7 +22,7 @@ def get_routing_agent() -> Agent:
 def get_strategic_advisor_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['StrategicAdvisor'],
-        llm=creative_openai_llm if not llm else llm,
+        llm=X_llm if not llm else llm,
         tools=[knowledge_service_tool, drill_down_topic_tool],
         verbose=True,
         allow_delegation=False,
@@ -31,7 +31,7 @@ def get_strategic_advisor_agent(llm=None) -> Agent:
 def get_system_operations_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['SystemOperationsAgent'],
-        llm=decivise_openai_llm if not llm else llm,
+        llm=X_llm if not llm else llm,
         tools=[system_operations_tool],
         verbose=True,
         allow_delegation=False,
@@ -40,7 +40,7 @@ def get_system_operations_agent(llm=None) -> Agent:
 def get_incremental_strategic_planner_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['IncrementalStrategicPlannerAgent'],
-        llm=creative_openai_llm if not llm else llm,
+        llm=X_llm if not llm else llm,
         tools=[knowledge_service_tool, drill_down_topic_tool],
         verbose=True,
         allow_delegation=False,
@@ -49,7 +49,7 @@ def get_incremental_strategic_planner_agent(llm=None) -> Agent:
 def get_communication_agent(llm=None) -> Agent:
     return Agent(
         config=agents_config['CommunicationAgent'],
-        llm=creative_openai_llm if not llm else llm,
+        llm=X_llm if not llm else llm,
         tools=[drill_down_topic_tool],
         verbose=True,
         allow_delegation=False,
@@ -58,7 +58,7 @@ def get_communication_agent(llm=None) -> Agent:
 def get_registration_agent() -> Agent:
     return Agent(
         config=agents_config['RegistrationDataCollectorAgent'],
-        llm=decivise_openai_llm,
+        llm=X_llm,
         verbose=True,
         allow_delegation=False
     )
@@ -66,7 +66,7 @@ def get_registration_agent() -> Agent:
 def get_history_summarizer_agent() -> Agent:
     return Agent(
         config=agents_config['HistorySummarizerAgent'],
-        llm=decivise_openai_llm,
+        llm=X_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -74,7 +74,7 @@ def get_history_summarizer_agent() -> Agent:
 def get_data_quality_agent() -> Agent:
     return Agent(
         config=agents_config['DataQualityAgent'],
-        llm=decivise_openai_llm,
+        llm=X_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -82,7 +82,7 @@ def get_data_quality_agent() -> Agent:
 def get_state_summarizer_agent() -> Agent:
     return Agent(
         config=agents_config['StateSummarizerAgent'],
-        llm=decivise_openai_llm,
+        llm=X_llm,
         verbose=True,
         allow_delegation=False,
     )
@@ -90,7 +90,7 @@ def get_state_summarizer_agent() -> Agent:
 def get_profile_enhancer_agent() -> Agent:
     return Agent(
         config=agents_config['ProfileEnhancerAgent'],
-        llm=creative_openai_llm,
+        llm=X_llm,
         verbose=True,
         allow_delegation=False,
     )
