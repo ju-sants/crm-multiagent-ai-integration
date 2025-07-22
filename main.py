68ec972bab9b0b32638a3e634aec7496f6f7af84
juan <juan.bisposantos05@gmail.com>
2025-07-22 08:53:41 -0300

    logging

--- main.py
+++ main.py
@@ -302,7 +302,10 @@ def receive_message():
         
         # Alessandro's team UUID
         if contact_info.get("team", {}).get("uuid") == "d468731afdba45c3a3a65895e4b08a5a":
+            # Salvando o timestamp da última mensagem recebida
             redis_client.set(f"history:last_timestamp:{contact_info['uuid']}", str(datetime.now().isoformat()))
+            
+            # Roteamento
             process_incoming_message(payload)
                 
     return jsonify({'status': 'ok'}), 200
