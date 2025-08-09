cf22698b079fde715edf16aebf7b5abc4e9760c0
juan <juan.bisposantos05@gmail.com>
2025-08-09 09:34:42 -0300

    Adicionando novo arquivo de patch, dessa vez para a telemetria do crewai, que algumas vezes da erro de conexão e interfere nas execuções das crews

---
+++ app/patches/crewai_telemetry_patch.py
@@ -0,0 +1,11 @@
+from crewai.telemetry import Telemetry
+
+def noop(*args, **kwargs):
+    # with open("./logfile.txt", "a") as f:
+    #     f.write("Telemetry method called and noop'd\n")
+    pass
+
+def apply_crewai_telemetry_patch():
+    for attr in dir(Telemetry):
+        if callable(getattr(Telemetry, attr)) and not attr.startswith("__"):
+            setattr(Telemetry, attr, noop)
