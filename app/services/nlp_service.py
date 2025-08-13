a995f2fe9cf63e62b39332126e6badf7fbb0fcec
juan <juan.bisposantos05@gmail.com>
2025-08-13 08:33:55 -0300

    utilizando outra estratégia de cache para lidar com múltiplas threads utilizando o modelo

--- app/services/nlp_service.py
+++ app/services/nlp_service.py
@@ -5,23 +5,12 @@ import os
 from app.core.logger import get_logger
 
 logger = get_logger(__name__)
-@lru_cache(maxsize=1)
+
+_modelo_semantico = None
 def carregar_modelo_semantico():
-    """
-    Carrega o modelo de análise semântica. 
-    Este modelo é otimizado para análise de similaridade e será baixado na primeira vez que o código for executado.
-    """
-    if not os.path.exists('sentence_transformers'):
-        os.makedirs('sentence_transformers')
-
-    logger.info("Carregando modelo de análise semântica...")
-    if os.path.exists('sentence_transformers/model'):
-        logger.info("Modelo já carregado.")
-        return SentenceTransformer('sentence_transformers/model')
-    else:
-        logger.info("Modelo não encontrado, baixando...")
-        modelo = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-        modelo.save('sentence_transformers/model')
-
-    logger.info("Modelo carregado.")
-    return modelo
+    global _modelo_semantico
+    if _modelo_semantico is None:
+        logger.info("Carregando modelo...")
+        _modelo_semantico = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+        logger.info("Modelo carregado.")
+    return _modelo_semantico
