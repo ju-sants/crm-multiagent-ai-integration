8ba4b1ba1b548e39176b0e2008343435e4d075e3
juan <juan.bisposantos05@gmail.com>
2025-08-28 16:55:03 -0300

    Criando função para carregar o modelo pre-treinado e fine-tunned BERTimbau. Da minha conta no huggingface. Para uma tarefa de NER (Named Entity Recognition)

--- app/services/nlp_service.py
+++ app/services/nlp_service.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer
+from transformers import pipeline
 from functools import lru_cache
 
 from app.core.logger import get_logger
@@ -11,3 +12,13 @@ def carregar_modelo_semantico():
     modelo_semantico = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
     logger.info("Modelo carregado.")
     return modelo_semantico
+
+@lru_cache(maxsize=1)
+def carregar_contact_name_extractor():
+    logger.info("Carregando modelo NER...")
+    ner = pipeline("ner", "ju-sants/contact-name-extractor")
+    logger.info("Modelo NER carregado...")
+
+    return ner
+
+
